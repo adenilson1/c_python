d118887b5d9244972c398b4daf5e5a7190fa401d
adenilson1 <adenilson99@hotmail.com>
2024-07-04 11:27:44 -0300

    fundamentos de python

---
+++ fundamentos_cont/aula78.py
@@ -0,0 +1,13 @@
+#calculando o primeiro digito
+
+cpf = '74682489070'
+nove_digitos = cpf[:9]
+contador_regressivo_1 = 10
+primeiro_digito = 0
+resultado_1 = 0
+
+for digito in nove_digitos:
+    resultado_1 +=(int(digito) * contador_regressivo_1)
+    contador_regressivo_1 -=1
+primeiro_digito = (resultado_1 * 10)%11
+print(primeiro_digito)
