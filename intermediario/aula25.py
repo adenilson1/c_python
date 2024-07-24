2f86566e4d095d29b561211b861f6d156df999c1
adenilson1 <adenilson99@hotmail.com>
2024-07-24 15:16:00 -0300

    conhecimento intermediário em pytho

--- intermediario/aula25.py
+++ intermediario/aula25.py
@@ -23,8 +23,8 @@ produtos = [
 ]
 
 novos_produtos = [
-    {**produto, 'preço':produto['preço'] * 1.05}
-    if produto ['preço'] > 20 else {**produto} #map
+    {**produto, 'preço':produto['preço'] * 1.05} #map
+    if produto ['preço'] > 20 else {**produto} 
     for produto in produtos
     if produto ['preço'] >=20 # filter
 ]
